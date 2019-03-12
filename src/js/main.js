02fdfc38630d60516eea96526c26f47a614f9f02
Igor Koshelev <igor.koshelev440@gmail.com>
2019-03-12 14:37:53 +0300

    day4

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,19 @@
 'use strict';
 
 (function() {
-var burger = document.querySelector('.header__burger');
-var menu = document.querySelector('.main-navigation');
-var closeButton = document.querySelector('.main-navigation__close');
 
-var closePopUp = function() {
-  menu.classList.add('no-display');
-  closeButton.removeEventListener('click', closePopUp)
-}
+  var burger = document.querySelector('.header__burger');
+  var menu = document.querySelector('.main-navigation');
+  var closeButton = document.querySelector('.main-navigation__close');
 
-burger.addEventListener('click', function() {
-  menu.classList.remove('no-display');
-  closeButton.addEventListener('click', closePopUp)
-})
+  var closePopUp = function() {
+    menu.classList.add('no-display');
+    closeButton.removeEventListener('click', closePopUp);
+  };
+
+  burger.addEventListener('click', function() {
+    menu.classList.remove('no-display');
+    closeButton.addEventListener('click', closePopUp);
+  });
 
 })();
