2b5c13364ed876e6403b8a8d69234046a4aa5e4d
Igor Koshelev <igor.koshelev440@gmail.com>
2019-05-22 16:19:24 +0300

    aside in menu

--- src/js/main.js
+++ src/js/main.js
@@ -4,22 +4,36 @@ try {
   (function() {
     var burger = document.querySelector('.header__burger');
     var menu = document.querySelector('.main-navigation');
-    // var closeButton = document.querySelector('.main-navigation__close');
+    var aside = document.querySelector('.aside-wrap');
 
     var closePopUp = function() {
       menu.classList.remove('main-navigation--opened');
       menu.classList.add('main-navigation--closed');
-      burger.classList.add('header__burger--open');
+
       burger.classList.remove('header__burger--close');
+      burger.classList.add('header__burger--open');
+
+      if (aside) {
+        aside.classList.remove('aside-wrap--unfold');
+        aside.classList.add('aside-wrap--fold');
+      }
+
       burger.removeEventListener('click', closePopUp);
       burger.addEventListener('click', openPopUp);
     };
 
     var openPopUp = function() {
-      menu.classList.add('main-navigation--opened');
       menu.classList.remove('main-navigation--closed');
+      menu.classList.add('main-navigation--opened');
+
       burger.classList.remove('header__burger--open');
       burger.classList.add('header__burger--close');
+
+      if (aside) {
+        aside.classList.remove('aside-wrap--fold');
+        aside.classList.add('aside-wrap--unfold');
+      }
+
       burger.removeEventListener('click', openPopUp);
       burger.addEventListener('click', closePopUp);
     };
