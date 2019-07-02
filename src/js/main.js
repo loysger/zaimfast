d1038dd65b841ebe20db2c00679b1d202ccb074d
Igor Koshelev <igor.koshelev440@gmail.com>
2019-07-02 20:37:34 +0300

    отображение дополнительных тарифов

--- src/js/main.js
+++ src/js/main.js
@@ -92,3 +92,32 @@ try {
     }
   })();
 } catch (error) {}
+
+try {
+  (function() {
+    var block = document.querySelector('.zaem-card');
+    var moreButton = document.querySelector('.zaem-card__tariffs-more');
+
+    var closeMenu = function() {
+      block.classList.remove('zaem-card_tariffs_opened');
+      block.classList.add('zaem-card_tariffs_closed');
+
+      moreButton.textContent = 'Еще тарифы';
+
+      moreButton.removeEventListener('click', closeMenu);
+      moreButton.addEventListener('click', openMenu);
+    };
+
+    var openMenu = function() {
+      block.classList.remove('zaem-card_tariffs_closed');
+      block.classList.add('zaem-card_tariffs_opened');
+
+      moreButton.textContent = 'Свернуть меню';
+
+      moreButton.removeEventListener('click', openMenu);
+      moreButton.addEventListener('click', closeMenu);
+    };
+
+    moreButton.addEventListener('click', openMenu);
+  })();
+} catch (error) {}
