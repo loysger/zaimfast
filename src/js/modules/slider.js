db80eff438465437f597317cf6d0860e25bd8245
Igor Koshelev <igor.koshelev440@gmail.com>
2019-08-27 16:47:28 +0300

    touch slider

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,3 +1,12 @@
+// <div class="slider">
+//   <div class="slider__conteiner">
+//     <div class="slider__lenght">
+//       <div class="slider__depth"></div>
+//       <div class="slider__pin"></div>
+//     </div>
+//   </div>
+// </div>
+
 export default class Slider {
   constructor(sliderElement) {
     this._element = sliderElement;
@@ -10,37 +19,80 @@ export default class Slider {
 
   _init() {
     this._pinElement.addEventListener(
-      `mousedown`,
-      this._startMouseTracking.bind(this)
+      'mousedown',
+      this._startTracking.bind(this)
+    );
+
+    // touch
+    this._pinElement.addEventListener(
+      'touchstart',
+      this._startTracking.bind(this)
     );
   }
 
-  _startMouseTracking(evt) {
+  _startTracking(evt) {
     evt.preventDefault();
-
     this._depthWidth = this._depthElement.clientWidth;
 
-    this._mouseUpHandler = this._stopMouseTracking.bind(this);
-    this._mouseMovementHandler = this._moveSlider.bind(this);
+    switch (evt.type) {
+      case 'touchstart':
+        this._touchCood = evt.touches[0].screenX;
 
-    document.addEventListener('mouseup', this._mouseUpHandler, {
-      once: true
-    });
-    document.addEventListener(`mousemove`, this._mouseMovementHandler);
+        this._touchStopHandler = this._stopTracking.bind(this);
+        this._touchMovementHandler = this._moveSlider.bind(this);
+
+        document.addEventListener('touchend', this._touchStopHandler, {
+          once: true
+        });
+        document.addEventListener(`touchmove`, this._touchMovementHandler);
+        break;
+
+      default:
+        this._mouseUpHandler = this._stopTracking.bind(this);
+        this._mouseMovementHandler = this._moveSlider.bind(this);
+
+        document.addEventListener('mouseup', this._mouseUpHandler, {
+          once: true
+        });
+        document.addEventListener(`mousemove`, this._mouseMovementHandler);
+        break;
+    }
   }
 
-  _stopMouseTracking() {
-    document.removeEventListener('mousemove', this._mouseMovementHandler);
+  _stopTracking(evt) {
+    switch (evt.type) {
+      case 'touchend':
+        document.removeEventListener('touchmove', this._touchMovementHandler);
+        break;
+
+      default:
+        document.removeEventListener('mousemove', this._mouseMovementHandler);
+        break;
+    }
   }
 
   _moveSlider(evt) {
     const style = this._depthElement.style;
-    const widthValue = this._depthWidth + evt.movementX;
     const maxWidthValue = this._lengthElement.clientWidth;
 
-    let percent;
+    let shift;
+
+    switch (evt.type) {
+      case 'touchmove':
+        shift = evt.touches[0].screenX - this._touchCood;
+        this._touchCood = evt.touches[0].screenX;
+        break;
+
+      default:
+        shift = evt.movementX;
+        break;
+    }
+
+    const widthValue = this._depthWidth + shift;
 
     this._depthWidth = widthValue;
+
+    let percent;
 
     if (widthValue <= 0 || widthValue >= maxWidthValue) {
       if (widthValue <= 0) {
@@ -49,7 +101,7 @@ export default class Slider {
         percent = 100;
       }
     } else {
-      percent = widthValue / maxWidthValue * 100;
+      percent = (widthValue / maxWidthValue) * 100;
     }
 
     style.width = `${percent}%`;
